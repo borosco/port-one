8c6cd77fc7a01a9ec0dccdb5f7fbfb8a551911eb
borosco <simpledarwins@gmail.com>
2020-02-12 15:13:06 +0300

    new initiation

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var add=function(numberOne,numberTwo){
+return numberOne+numberTwo;
+}
+numberOne=parseInt(prompt("please enter a number"));
+numberTwo=parseInt(prompt("please enter a second number"));
+alert(add(numberOne,numberTwo));
